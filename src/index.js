c05ffdae146906e2f5df3fb9a1817068eddb1296
Harold Torres Marino <haroldtm55@gmail.com>
2021-11-19 06:35:47 +1100

    commit tree 96edb774c0c23adeec1e58832fea1d16fa5ab7be

--- src/index.js
+++ src/index.js
@@ -19,4 +19,28 @@ function render() {
 };
 
 // Use your createStore function and the functions provided here to create a store.
+
+function createStore(reducer) {
+  let state
+
+  function dispatch(action) {
+    state = reducer(state, action)
+    render()
+  }
+
+  function getState() {
+    return state
+  }
+
+  return {
+    dispatch,
+    getState
+  }
+}
+
+let store = createStore(candyReducer)
+
 // Once the store is created, call an initial dispatch.
+
+
+store.dispatch({type: "bLA"})
